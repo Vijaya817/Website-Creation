39ea53dc77d848244022b4e04bdbce938505df41
Vijaya817 <viji101097@gmail.com>
2026-04-29 13:49:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+<script>
+function scrollToSection(){
+    document.getElementById("contact").scrollIntoView({behavior:"smooth"});
+}
+
+function submitForm(){
+    alert("Message sent successfully!");
+    return false;
+}
+
+
+</script>
